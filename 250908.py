048146b6c9c68582cc81366a5671a857830e9543
songjunseo-ai <songjunseo03710@gmail.com>
2025-09-08 15:13:43 +0900

    Update 250908.py

--- 250908.py
+++ 250908.py
@@ -1,9 +1,4 @@
-names = ['Alice', 'Bob', 'Charlie']
-ages = [25, 30, 35]
-
-for  (name, age) in enumerate(zip(names, ages), start=1):
-    print(f"{idx}. {name} - {age}세")
-print("새우먹고싶다")
-print("짬뽕먹고싶다")
-print("짜장면먹고싶다")
+print("잠 하루종일 자고싶다")
+print("헬스장 가고싶다")
+print("집가고싶다")
 
